5b8035a6774392e18189029fe9791139607b3bce
Jonathan Rappner <jrappner@gmail.com>
2021-06-23 17:37:36 +0200

    dynamic config

---
+++ src/config.js
@@ -0,0 +1,15 @@
+// config for development
+const config_dev = {
+	"api_url": "http://localhost:4000/"
+}
+
+// config for production
+const config_prod  = {
+	"api_url": "https://screens-api.aeoah.se/"
+}
+
+
+// choose the config object
+module.exports = process.env.NODE_ENV === 'development' 
+	? config_dev
+	: config_prod
