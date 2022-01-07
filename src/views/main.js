bdf03b15f92f69cd0a7a91d0b379f2ad7acb3ffc
Jonathan Rappner <jrappner@gmail.com>
2022-01-07 11:48:52 +0100

    favicon

--- src/views/main.js
+++ src/views/main.js
@@ -16,6 +16,7 @@ const Main = () => {
 	const { param1, param2 } = useParams()
 	const navigate = useNavigate()
 	const [screen, setScreen] = useState()
+	const [gameFilterIcon, setGameFilterIcon] = useState('favicon.ico') // Store current favicon based on game filter. Use 'favicon.ico' when no filter is active. The viewer overrides this and doesn't use it.
 	const [thumbs, setThumbs] = useState([])
 
 	// URL-parametrar
@@ -24,22 +25,41 @@ const Main = () => {
 
 	// gameCode changes
 	useEffect(() => {
+		// get thumbnails
 		axios.get(`${config.api_url}screens/${gameCode ?? 'all'}/latest/${100}`)////////////100
 			.then(res => {
 				setThumbs(res.data)
 			}).catch(console.error)
+
+		// get game-info
+		if (gameCode) {
+			axios.get(`${config.api_url}games/${gameCode}`)
+				.then(res => {
+					const icon = 'data:image/png;base64,' + res.data.icon16
+					setGameFilterIcon(icon)
+					document.getElementById("favicon").href = icon
+				}).catch(console.error)
+		} else {
+			setGameFilterIcon('favicon.ico') // if no filter, set to default
+		}
 	}, [gameCode])
 
 	// screenId changes
 	useEffect(() => {
 		if (typeof screenId !== 'undefined') {
-			
-			// ladda screen info från api
+
+			// load screen info from api
 			axios.get(`${config.api_url}screens/${screenId}`)
 				.then(res => {
 					setScreen(res.data) // state
-					document.getElementById("favicon").href = res.data.game.icon48_url // favicon
 					document.title = `${res.data.game.code.toUpperCase()}: ${res.data.date_time.format_long}` // title
+
+					// get the screen's game's icon
+					axios.get(`${config.api_url}games/${res.data.game.code}`)
+						.then(res => {
+							document.getElementById("favicon").href = 'data:image/png;base64,' + res.data.icon16
+						}).catch(console.error)
+
 				}).catch(console.error)
 		}
 	}, [screenId])
@@ -48,7 +68,7 @@ const Main = () => {
 		navigate(`/${gameCode ?? ''}`)
 		setScreen()
 		document.body.style.overflow = 'auto' // enable scrolling
-		document.getElementById("favicon").href = 'favicon.ico' // restore favicon
+		document.getElementById("favicon").href = gameFilterIcon // restore favicon
 		document.title = 'Screens' // restore title
 	}
 
